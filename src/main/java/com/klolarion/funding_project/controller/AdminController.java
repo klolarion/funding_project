27c95fcdfae31926c98b4219196648e5320bec4c
윤효정 <0213yhj@naver.com>
2024-08-12 16:44:23 +0900

    addProduct

--- src/main/java/com/klolarion/funding_project/controller/AdminController.java
+++ src/main/java/com/klolarion/funding_project/controller/AdminController.java
@@ -1,9 +1,6 @@
 package com.klolarion.funding_project.controller;
 
-import com.klolarion.funding_project.service.AdminService;
-import com.klolarion.funding_project.service.AdminServiceImpl;
-import com.klolarion.funding_project.service.MemberService;
-import com.klolarion.funding_project.service.MemberServiceImpl;
+import com.klolarion.funding_project.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.http.ResponseEntity;
@@ -16,17 +13,19 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequiredArgsConstructor
 @RequestMapping("/admin")
 public class AdminController {
-    private final AdminService adminService;
     private final AdminServiceImpl adminServiceImpl;
+    private final ProductServiceImpl productServiceImpl;
     private MemberServiceImpl memberServiceImpl;
 
     @GetMapping
     public String admin(Model model){
+        model.addAttribute("products",productServiceImpl.allProducts() );
         return "admin";
     }
 
     @PostMapping("/add")
     public String addProduct(@RequestParam String productName, @RequestParam Long price, @RequestParam int stuck) {
+        adminServiceImpl.addProduct(productName, price, stuck);
         return "redirect:/admin";
     }
 
