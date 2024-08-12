71fca1b1f814f99f1a99f6a74ee121ce45cc85d6
Klolarion <85093525+klolarion@users.noreply.github.com>
2024-08-12 11:22:47 +0900

    feature/service-FriendService&Impl -JK
    친구서비스 구현.
    조회용 Dto 추가

--- src/main/java/com/klolarion/funding_project/service/FriendService.java
+++ src/main/java/com/klolarion/funding_project/service/FriendService.java
@@ -1,18 +1,27 @@
 package com.klolarion.funding_project.service;
 
 import com.klolarion.funding_project.domain.entity.Friend;
+import com.klolarion.funding_project.dto.FriendDto;
+
+import java.util.List;
 
 public interface FriendService {
 
     /*친구추가*/
-    Friend addFriend();
+    Friend addFriend(Long accepterId);
+
+    /*친구 목록*/
+    List<FriendDto> friendList();
+
+    /*친구 요청 목록*/
+    List<FriendDto> requestList();
 
     /*친구요청 수락*/
-    boolean acceptFriendRequest();
+    boolean acceptFriendRequest(Long friendId);
 
     /*친구삭제*/
-    boolean removeFriend();
+    boolean removeFriend(Long friendId);
 
-    /*차단*/
-    boolean banMember();
+    /*친구 차단 or 친구요청 거부시 차단*/
+    boolean banMember(Long memberId);
 }
