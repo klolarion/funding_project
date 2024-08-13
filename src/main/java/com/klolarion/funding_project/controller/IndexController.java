35c04807aff1db28e3beea662f46615a3a6850cb
윤효정 <0213yhj@naver.com>
2024-08-13 09:37:06 +0900

    update paymentMethod

--- src/main/java/com/klolarion/funding_project/controller/IndexController.java
+++ src/main/java/com/klolarion/funding_project/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.klolarion.funding_project.controller;
 import com.klolarion.funding_project.domain.entity.Member;
 import com.klolarion.funding_project.domain.entity.Role;
 import com.klolarion.funding_project.service.FundingServiceImpl;
+import com.klolarion.funding_project.service.MemberServiceImpl;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
