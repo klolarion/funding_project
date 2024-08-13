1a87ec5ef4cf9e7023cbc7f6296c51b4c46ec265
Klolarion <85093525+klolarion@users.noreply.github.com>
2024-08-13 11:05:11 +0900

    aws test

--- src/main/java/com/klolarion/funding_project/dto/LoginDto.java
+++ src/main/java/com/klolarion/funding_project/dto/LoginDto.java
@@ -1,6 +1,13 @@
 package com.klolarion.funding_project.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginDto {
-    private String email;
+    private String account;
     private String password;
 }
