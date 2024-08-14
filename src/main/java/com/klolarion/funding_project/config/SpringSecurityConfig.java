c0c53613b945a3379c1df21d568442b475221f0a
Klolarion <85093525+klolarion@users.noreply.github.com>
2024-08-14 15:54:33 +0900

    for test

--- src/main/java/com/klolarion/funding_project/config/SpringSecurityConfig.java
+++ src/main/java/com/klolarion/funding_project/config/SpringSecurityConfig.java
@@ -30,28 +30,11 @@ public class SpringSecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 안 함
                 )
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/login", "/opt", "/v3/api-docs/**", "/swagger-ui/**").permitAll() // 특정 경로는 모든 사용자에게 허용
-                        .requestMatchers("/f0").hasRole("ADMIN")
                         .anyRequest().authenticated() // 나머지 요청은 인증 필요
                 )
                 .authenticationProvider(customAuthenticationProvider) // 사용자 정의 인증 프로바이더 설정
                 .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class) // 사용자 정의 필터 추가
-                .exceptionHandling(exception -> exception
-                        .accessDeniedHandler((request, response, accessDeniedException) -> {
-                            // 권한 문제가 발생했을 때 호출
-                            response.setStatus(403);
-                            response.setCharacterEncoding("utf-8");
-                            response.setContentType("text/html; charset=UTF-8");
-                            response.getWriter().write("권한이 없는 사용자입니다.");
-                        })
-                        .authenticationEntryPoint((request, response, authException) -> {
-                            // 인증문제가 발생했을 때 호출
-                            response.setStatus(401);
-                            response.setCharacterEncoding("utf-8");
-                            response.setContentType("text/html; charset=UTF-8");
-                            response.getWriter().write("인증되지 않은 사용자입니다.");
-                        })
-                );
+        ;
         return http.build();
     }
 
