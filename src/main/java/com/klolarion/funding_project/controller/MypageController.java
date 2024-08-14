72e7804adf6fd8b2f05de756e0d2a602f9a37339
윤효정 <0213yhj@naver.com>
2024-08-14 10:52:31 +0900

    update group

--- src/main/java/com/klolarion/funding_project/controller/MypageController.java
+++ src/main/java/com/klolarion/funding_project/controller/MypageController.java
@@ -1,6 +1,7 @@
 package com.klolarion.funding_project.controller;
 
 import com.klolarion.funding_project.service.FundingServiceImpl;
+import com.klolarion.funding_project.service.GroupServiceImpl;
 import com.klolarion.funding_project.service.PaymentServiceImpl;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -17,11 +18,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MypageController {
 //    private final PaymentServiceImpl paymentServiceImpl;
     private final FundingServiceImpl fundingServiceImpl;
+    private final GroupServiceImpl groupServiceImpl;
 
     @GetMapping
     public String myPage(HttpSession session, Model model) {
         model.addAttribute("member", session.getAttribute("member") );
         model.addAttribute("myFundingList", fundingServiceImpl.myFundingList());
+//        model.addAttribute("myGroupList", groupServiceImpl.myGroups());
+//        model.addAttribute("groupList", groupServiceImpl)
         return "myPage";
     }
 
